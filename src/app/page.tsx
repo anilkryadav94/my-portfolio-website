f9baec0089b6c171e58b2e54cb2aae8569200735
Anil kumar <ayadav232@gmail.com>
2025-07-18 08:45:57 +0000

    add margin to the left of the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,16 +10,18 @@ import Footer from '@/components/landing/footer';
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-background">
-      <Header />
-      <main className="flex-1">
-        <Hero />
-        <About />
-        <Skills />
-        <Experience />
-        <ResumeTailor />
-        <ContactForm />
-      </main>
-      <Footer />
+      <div className="pl-8">
+        <Header />
+        <main className="flex-1">
+          <Hero />
+          <About />
+          <Skills />
+          <Experience />
+          <ResumeTailor />
+          <ContactForm />
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }
