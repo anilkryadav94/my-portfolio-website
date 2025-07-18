e8ac817bc9baec6374ce824eeb1cca077c549147
Anil kumar <ayadav232@gmail.com>
2025-07-18 15:37:54 +0000

    Header me Anil's portfolio likha hai use thoda right kro.. margin left l

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -21,7 +21,7 @@ export default function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center justify-between">
-        <a href="#" className="font-headline text-lg font-bold text-primary ml-0.5">
+        <a href="#" className="font-headline text-lg font-bold text-primary ml-1">
           Anil's Portfolio
         </a>
         
