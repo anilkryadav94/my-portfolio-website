4918b25b8e58a234cb3c322a96ba3e3e6426f9ed
Anil kumar <ayadav232@gmail.com>
2025-07-18 08:47:11 +0000

    little less.. it should be same in left and also in right

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import Footer from '@/components/landing/footer';
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-background">
-      <div className="pl-8">
+      <div>
         <Header />
         <main className="flex-1">
           <Hero />
